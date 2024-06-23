67e10ae8f8397e18b79e9c0f739cacc85738e573
drimcaster <drimcaster@gmail.com>
2024-06-23 21:57:46 +0800

    Update DataModelController.php

--- src/Http/Controllers/DataModelController.php
+++ src/Http/Controllers/DataModelController.php
@@ -16,7 +16,7 @@ class  DataModelController extends _CommonController
     //
     public $guard = 'admin';
     public function index(Request $request){
-        return $this->view('iprotek_data::searches.index');
+        return $this->view('iprotek_data::manage.searches.index');
 
     }
 
