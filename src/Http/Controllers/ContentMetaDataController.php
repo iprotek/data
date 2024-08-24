c1ea1a4f85fa1200ff885076f58646e70d35d730
drimcaster <drimcaster@gmail.com>
2024-08-24 16:28:57 +0800

    update

--- src/Http/Controllers/ContentMetaDataController.php
+++ src/Http/Controllers/ContentMetaDataController.php
@@ -4,9 +4,46 @@ namespace iProtek\Data\Http\Controllers;
 
 use Illuminate\Http\Request;
 use iProtek\Core\Http\Controllers\_Common\_CommonController;
+use Illuminate\Routing\Controller as BaseController;  
 use iProtek\Core\Helpers\PayModelHelper;
+use iProtek\Data\Models\ContentMetaData; 
 
-class ContentMetaDataController extends Controller
+class ContentMetaDataController extends BaseController
 {
     //
+    protected $guard = 'admin';
+
+    public function get_info(Request $request, $id){
+        /*
+        $meta = ContentMetaData::find($id);
+        if(!$meta)
+            abort(403, 'Not Found');
+
+        //*/
+        $source = $request->souce;
+        
+        $id = PayModelHelper::get_own(ContentMetaData::class, $request, [
+            "source"=>$source
+        ])->first();
+
+
+        return $id;
+    }
+
+    public function add(Request $request){
+
+        return ["status"=>1, "message"=>"Successfully added MetaData"];
+    }
+
+    public function update(Request $request){
+
+        return ["status"=>1, "message"=>"Successfully updated MetaData"];
+    }
+
+    public function remove(Request $request){
+
+        return ["status"=>1, "message"=>"Successfully removed MetaData"];
+    }
+
+
 }
--- src/Models/ContentMetaData.php
+++ src/Models/ContentMetaData.php
@@ -5,6 +5,7 @@ namespace iProtek\Data\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use iProtek\Core\Models\FileUpload;
 
 class ContentMetaData extends Model
 {
@@ -25,4 +26,8 @@ class ContentMetaData extends Model
     public $casts = [
         "meta_data"=>"json"
     ];
+
+    public function profile_image(){
+        return $this->hasMany(FileUpload::class, );
+    }
 }
