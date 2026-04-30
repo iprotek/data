7a25d8f473fac0306ce25cb8be9a72c29163a923
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-04-30 21:47:45 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use iProtek\Data\Http\Controllers\ModelFieldController;
  
 Route::prefix('api')->middleware(['api'])->group(function(){
     
-    Route::prefix('group/{group_id}')->middleware(['pay.api'])->name('api')->group(function(){
+    Route::prefix('group/{group_id}')->middleware(['pay.api', 'policy.control'])->name('api')->group(function(){
         
         //FILE UPLOADS
         //Route::prefix('data')
